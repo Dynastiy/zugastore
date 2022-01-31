12ea98b94bae166d13b0daa58cd252500a3bb546
Dynastiy <emmanuel00michael@gmail.com>
2022-01-31 02:18:34 +0100

    fixed all, new and popular products

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,15 +1,51 @@
 <template>
   <div>
-    <div class="d-flex justify-content-between align-items-center">
-      <h6 class="font-weight-bold">ALL PRODUCTS</h6>
-      <!-- <p class="text-info">More <i class="fa fa-angle-right" aria-hidden="true"></i> </p> -->
-    </div>
-    <hr class="hr my-4" />
-    <CarouselHeader />
+    <CarouselHeader /> 
+    <div class="">
+        <h6 class="font-weight-bold">NEW PRODUCTS</h6>
+      </div>
+      <hr class="hr my-4" />
     <div class="new__apps">
+     
       <div
         class=" text-center"
-        v-for="product in products"
+        v-for="new_product in products"
+        :key="new_product.id"
+      >
+        <router-link :to="'/product/' + new_product.id">
+          <div class="app__list p-2 ">
+            <img
+              :src="
+                'https://api.africanapp.store/softwares/photos/' + new_product.app_icon
+              "
+              alt=""
+              width="50"
+              height="50"
+            />
+            <div class="">
+              <h4 class="text-dark mt-2 font-weight-bold">
+                {{ new_product.name }}
+              </h4>
+
+              <h4 class="small py-2 bg-dark w-100 text-info font-weight-bold text-capitalize">
+                {{ new_product.file_size }}
+              </h4>
+            </div>
+          </div>
+        </router-link>
+      </div>
+    </div>
+
+    <!-- Popular Apps --> 
+    <div class="mt-4">
+        <h6 class="font-weight-bold">POPULAR PRODUCTS</h6>
+      </div>
+      <hr class="hr my-4" />
+    <div class="new__apps">
+     
+      <div
+        class=" text-center"
+        v-for="product in popular"
         :key="product.id"
       >
         <router-link :to="'/product/' + product.id">
@@ -35,6 +71,42 @@
         </router-link>
       </div>
     </div>
+
+    <!-- All Products --> 
+    <div class=" mt-4">
+        <h6 class="font-weight-bold">ALL PRODUCTS</h6>
+      </div>
+      <hr class="hr my-4" />
+    <div class="new__apps">
+     
+      <div
+        class=" text-center"
+        v-for="all_product in all_products"
+        :key="all_product.id"
+      >
+        <router-link :to="'/product/' + all_product.id">
+          <div class="app__list p-2 ">
+            <img
+              :src="
+                'https://api.africanapp.store/softwares/photos/' + all_product.app_icon
+              "
+              alt=""
+              width="50"
+              height="50"
+            />
+            <div class="">
+              <h4 class="text-dark mt-2 font-weight-bold">
+                {{ all_product.name }}
+              </h4>
+
+              <h4 class="small py-2 bg-dark w-100 text-info font-weight-bold text-capitalize">
+                {{ all_product.file_size }}
+              </h4>
+            </div>
+          </div>
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -47,6 +119,8 @@ export default {
   data() {
     return {
       products: [],
+      popular: [],
+      all_products:[],
     };
   },
   methods: {
@@ -55,6 +129,11 @@ export default {
         const res = await this.$store.dispatch("getProducts");
         console.log(res.products);
         this.products = res.products;
+        console.log(this.products);
+        this.popular = res.popular_products;
+        console.log(this.popular);
+        this.all_products = res.all_products.data
+        console.log(this.all_products);
       } catch (error) {
         console.log(error);
       }
